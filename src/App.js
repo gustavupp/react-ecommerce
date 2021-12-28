fa04a8ed9e11ff20bfd01746b8772adc5288a151
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-28 19:18:44 +1000

    basic react-router-dom setup finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import  {commerce}  from './lib/commerce'
-import {useEffect, useState} from 'react'
+//import  {commerce}  from './lib/commerce'
+import Home from './Home';
+import Navbar from './Navbar';
+import  Error from './Error'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-const [products, setProducts] = useState([]);
-
-  const fetchProducts = async () => {
-    try {
-      const { data } = await commerce.products.list();
-      setProducts(data);
-    } catch {
-      console.log('there has been an error fetching data from the API')
-    }
-  }
-  console.log(products)
-
-  useEffect(() => {
-    fetchProducts();
-  },[])
-
-  return <h1>App</h1>
+  return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route exact path='/'>
+          <Home /> 
+        </Route>
+        <Route exact path='*'>
+          <Error /> 
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
+
+ // const [products, setProducts] = useState([]);
+
+  // const fetchProducts = async () => {
+  //   try {
+  //     const { data } = await commerce.products.list();
+  //     setProducts(data);
+  //   } catch {
+  //     console.log('there has been an error fetching data from the API')
+  //   }
+  // }
+  // console.log(products)
+
+  // useEffect(() => {
+  //   fetchProducts();
+  // },[])
